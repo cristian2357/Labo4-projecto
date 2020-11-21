199713741a4172510cfa49e4be8a467376b03db9
Cristian <cristian.acosta2357@gmail.com>
2020-11-21 16:30:35 -0300

    Mas cambios en directorios

--- cfg/Configuration.php
+++ cfg/Configuration.php
@@ -5,9 +5,25 @@ class Configuration
     const DB_USER = "b1fba2bae6cecb";
     const DB_PASSWORD = "6a3e69e6";
     const DB_NAME = "heroku_49ccb216faf4052";
+    const NOMBRE_PROYECTO = "Labo4 projecto";
 
     public static function getAbsolutePath()
     {
-        return str_replace('\\', '/', getcwd());        
+        return str_replace('\\', '/', getcwd()) . '/..';
+    }
+
+    public static function getAbsolutePathSinDirectorioLocal()
+    {
+        $path = self::getAbsolutePath();
+        $dirs = explode('/', $path);
+        $output = "";
+        $encontrado = false;
+        foreach ($dirs as $d) {
+            if ($encontrado)
+                $output .= $d;
+            if ($d == self::NOMBRE_PROYECTO)
+                $encontrado = true;
+        }
+        return $output;
     }
 }
