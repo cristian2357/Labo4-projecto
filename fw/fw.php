9945f6e0c69d9c9630cd4fdb6621ebb4a15cd2fc
Cristian <cristian.acosta2357@gmail.com>
2020-12-11 21:15:18 -0300

    include

--- fw/fw.php
+++ fw/fw.php
@@ -5,3 +5,5 @@
 require Configuration::getAbsolutePath().'/fw/Database.php';
 require Configuration::getAbsolutePath().'/fw/Model.php';
 require Configuration::getAbsolutePath().'/fw/View.php';
+require Configuration::getAbsolutePath().'/models/Exceptions/DefaultException.php';
+require Configuration::getAbsolutePath().'/models/Exceptions/validacionexception.php';
